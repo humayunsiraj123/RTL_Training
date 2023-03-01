b40900ee2ccaf928ceb1e6ff2c2ffba5e6754e15
Humayun Sheikh <58903281+humayunsiraj123@users.noreply.github.com>
2023-03-01 12:06:55 +0500

    Update testbench.sv

--- Array/testbench.sv
+++ Array/testbench.sv
@@ -1,5 +1,3 @@
-module tb;
-  bit [7:0] data;
    //dynamic array
   bit [7:0]data1;
   
@@ -15,7 +13,22 @@ module tb;
     for(int i=0; i < $size(data1);i++)begin 
       $display("the value  on index %d is %b = %b",i,data1[i],data[i]);
     end
+    $display("the facotrial of %d  is %d by recursion",8,factorial(8),fact(8));
 
   end
+  function int factorial(int n); 
+    int result = 1;
+    repeat(n)
+      result = result *(n--);
+    return result;
+    endfunction
+  
+  function int fact(int n);
+    if(n>0)
+      return n*fact(n-1);
+    else
+      return 1;
+  endfunction
+  
     endmodule
-    
+   
