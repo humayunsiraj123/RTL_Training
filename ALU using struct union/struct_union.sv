7d518fa35e5b5c386d4c81d849a1c3c7659222b1
Humayun Sheikh <58903281+humayunsiraj123@users.noreply.github.com>
2023-03-07 15:29:21 +0500

    Add files via upload

---
+++ ALU using struct union/struct_union.sv
@@ -0,0 +1,41 @@
+
+typedef enum {add,sub,mul,div,sl,sr} opcode_t;
+typedef enum {sign,unsign} operand_type_t;
+
+typedef union packed{ 
+logic [31:0] u_data;
+bit signed [31:0] s_data;
+} data_t;
+
+typedef struct packed {
+opcode_t opc;
+operand_type_t op_type;
+data_t op_a;
+data_t op_b;
+} instr_t;
+
+
+module struct_union(input instr_t IW,output data_t alu_out);
+always@(IW) begin
+if(IW.op_type==sign)begin
+case(IW.opc)
+add : alu_out.s_data=IW.op_a.s_data + IW.op_b.s_data;
+sub : alu_out.s_data=IW.op_a.s_data - IW.op_b.s_data;
+div : alu_out.s_data=IW.op_a.s_data / IW.op_b.s_data; 
+mul : alu_out.s_data=IW.op_a.s_data * IW.op_b.s_data;
+sl  : alu_out.s_data=IW.op_a.s_data<<<1 		    ;
+sr  : alu_out.s_data=IW.op_a.s_data>>>1 		    ;
+endcase 
+end
+if(IW.op_type==unsign)begin
+case(IW.opc)
+add : alu_out.u_data=IW.op_a.u_data + IW.op_b.u_data;
+sub : alu_out.u_data=IW.op_a.u_data - IW.op_b.u_data;
+div : alu_out.u_data=IW.op_a.u_data / IW.op_b.u_data; 
+mul : alu_out.u_data=IW.op_a.u_data * IW.op_b.u_data;
+sl  : alu_out.u_data=IW.op_a.u_data<<<2  	    ;
+sr  : alu_out.u_data=IW.op_a.u_data>>>2 	;
+endcase 
+end
+end
+endmodule;
---
+++ ALU using struct union/tb_struct_union.sv
@@ -0,0 +1,73 @@
+module tb_struct_union;
+
+logic[31:0] a,b,c;
+bit signed [31:0]s_a,s_b,s_c;
+
+typedef enum bit[2:0] {add,sub,mul,div,sl,sr} opcode_t;
+typedef  enum {sign,unsign} operand_type_t;
+
+typedef union packed{
+logic [31:0] u_data;
+bit signed[31:0] s_data;
+}data_t;
+typedef struct packed{
+opcode_t opr;
+operand_type_t opr_type;
+data_t opr_a;
+data_t opr_b;
+}instr_t;
+instr_t IW;
+data_t result;
+
+struct_union alu1(IW,result);
+
+
+initial begin
+#5;
+IW.opr_a='h10;
+IW.opr_b='h20;
+IW.opr_type=sign;
+IW.opr=add;
+display();
+#100;
+IW.opr=IW.opr.next();
+display();
+#100;
+IW.opr=IW.opr.next();
+display();
+#100;
+IW.opr=IW.opr.next();
+display();
+#100;
+IW.opr=IW.opr.next();
+display();
+#100;
+IW.opr=IW.opr.next();
+
+#100;
+IW.opr=IW.opr.next();
+display();
+#100;
+IW.opr=IW.opr.next();
+display();
+repeat(10) 
+begin
+#100;
+randomizer();
+
+end
+end
+task display();
+$display("A= %d  B = %d  operation %s datatype  $s   result = %p",IW.opr_a,IW.opr_b,IW.opr.name(),IW.opr_type.name(),result);
+endtask
+
+task randomizer();
+IW.opr_a=$random();
+IW.opr_b=$random();
+IW.opr_type=sign;
+IW.opr=IW.opr.next();
+display();
+endtask
+
+endmodule
+ 
