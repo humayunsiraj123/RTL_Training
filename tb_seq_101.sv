99772f80b70f872400b599d1eec2b3f15c9a8095
Humayun Sheikh <58903281+humayunsiraj123@users.noreply.github.com>
2023-03-29 10:04:32 +0500

    Create tb_seq_101.sv

---
+++ tb_seq_101.sv
@@ -0,0 +1,25 @@
+module tb_seq_101;
+  logic clk;
+  logic rst_n;
+  logic data_in;
+  logic out;
+  
+  
+  always #5 clk=~clk;
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,tb_seq_101);
+  rst_n=1'b0;
+  repeat(25) @(posedge clk);
+  rst_n=1'b1;
+  repeat(5) @(posedge clk);
+    repeat(100) begin 
+    data_in =$random;
+      if( out === 1)
+        $display("the output is 1 at time %0t",$time);
+      @(posedge clk);
+    end
+    $stop;
+  end
+endmodule
