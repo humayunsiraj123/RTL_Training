9876450e98ac7ae5c5d68bc2b6776f36992c5c00
Humayun Sheikh <58903281+humayunsiraj123@users.noreply.github.com>
2023-03-29 15:36:44 +0500

    customize tb for nbit bin to grey converter

--- DAY3/tb_bin_to_grey.sv
+++ DAY3/tb_bin_to_grey.sv
@@ -2,11 +2,14 @@
 // or browse Examples
 
 module tb_bin_to_grey ;
+  localparam N=8;
   reg  [4:0] bin  ;
   wire [4:0] grey ;
+  reg  [N-1:0] bin_n  ;
+  wire [N-1:0] grey_n ;
   
   bin_to_grey dut(.bin(bin),.grey(grey));
-  
+  bin_to_grey#(.N(N)) dut1(.bin(bin_n),.grey(grey_n));
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars(1);
@@ -33,9 +36,19 @@ module tb_bin_to_grey ;
           result         ;
         
       end
+    repeat(10)
+      begin
+      	#10
+          bin_n = $random  ;
+          result_n         ;
+        
+      end
     
   end
   function result;
   $display ("The value of GREY CODE OF BIN %b  = %b",bin,grey);
+  endfunction
+  function result_n;
+    $display ("The value of GREY CODE OF BIN %b  = %b",bin_n,grey_n);
   endfunction
 endmodule   
