76283657cd816e72b1411ec11b785c5f78500c24
Humayun Sheikh <58903281+humayunsiraj123@users.noreply.github.com>
2023-03-29 12:22:04 +0500

    Add files via upload

---
+++ divisible by 5 fsm/design.sv
@@ -0,0 +1,144 @@
+// Code your design here
+module div_by_5(
+  input clk,
+  input rst_n,
+  input data_in,
+  output out);
+  
+  typedef enum int {A=0,B,C,D,E} STATE ;
+  STATE nxt_state,cur_state;
+  
+  logic [31:0] reg_temp;
+  logic [31:0] nxt_temp;
+  logic [2:0] nxt_remain;
+  logic [2:0] reg_remain;
+  
+  
+  always_ff @(posedge clk)
+    begin
+      if(~rst_n)
+        cur_state <=cur_state.first();
+      else
+        cur_state <= nxt_state;
+    end
+  
+ always_ff @(posedge clk)
+    begin
+      if(~rst_n)
+        reg_temp <=0;
+      else
+       reg_temp <=nxt_temp;
+    end
+ always_ff @(posedge clk)
+    begin
+      if(~rst_n)
+        reg_remain <=0;
+      else
+       reg_remain <=nxt_remain;
+    end
+  
+  
+  always_comb
+    begin
+      case(cur_state)
+        A : nxt_state = STATE'(reg_remain);
+    	B : nxt_state = STATE'(reg_remain);
+		C : nxt_state = STATE'(reg_remain);
+		D : nxt_state = STATE'(reg_remain);
+		E : nxt_state = STATE'(reg_remain);
+      endcase
+        end
+  
+ always_comb
+    begin
+      case(cur_state)
+        A : out = (rst_n ==1'b0 && data_in === 1'bx) ? 1'b0 :1'b1;
+    	B : out = 1'b0;
+		C : out = 1'b0;
+		D : out = 1'b0;
+		E : out = 1'b0;
+        default out =1'b0;
+      endcase
+        end
+  assign nxt_temp = {reg_temp[30:0],data_in};
+  assign nxt_remain = reg_remain % 5;
+  
+endmodule
+
+
+
+
+
+module div_by_5_simple(
+  input clk,
+  input rst_n,
+  input data_in,
+  output out);
+  
+ enum logic[2:0]{A,B,C,D,E} nxt_state,cur_state;
+
+  
+  
+  always_ff @(posedge clk)
+    begin
+      if(~rst_n)
+        cur_state <=cur_state.first();
+      else
+        cur_state <= nxt_state;
+    end
+  
+
+  
+  
+  always_comb
+    begin
+      case(cur_state)
+        A : begin
+          if(data_in ==1'b1) 
+          	nxt_state = B ;
+       	 else
+           nxt_state  = A;
+        	end
+    	B : begin
+          if(data_in == 1'b0) 
+          	nxt_state = C ;
+       	 else
+           nxt_state  = D;
+        	end
+        C : begin
+          if(data_in ==1'b1) 
+          	nxt_state = A ;
+       	 else
+           nxt_state  = E;
+        	end
+		D : begin
+          if(data_in ==1'b1) 
+          	nxt_state = C ;
+       	 else
+           nxt_state  = B;
+        	end
+		E : begin
+          if(data_in ==1'b1) 
+          	nxt_state = E ;
+       	 else
+           nxt_state  = D;
+        	end
+      endcase
+        end
+  
+ always_comb
+    begin
+      case(cur_state)
+        A : out = (rst_n ==1'b0 && data_in === 1'bx) ? 1'b0 :1'b1;
+    	B : out = 1'b0;
+		C : out = 1'b0;
+		D : out = 1'b0;
+		E : out = 1'b0;
+        default out =1'b0;
+      endcase
+        end
+
+  
+endmodule
+
+    
---
+++ divisible by 5 fsm/testbench.sv
@@ -0,0 +1,61 @@
+// Code your testbench here
+// or browse Examples
+module tb_div_by_5;
+  logic clk;
+  logic rst_n;
+  logic data_in;
+  logic out;
+  logic fsm2;
+  
+  
+  div_by_5_simple DUT1(
+	clk,
+	rst_n,
+	data_in,
+  	fsm2);
+  
+   div_by_5 DUT2(
+clk,
+rst_n,
+ data_in,
+out);
+  
+  
+  always #5 clk =~ clk;
+  
+  
+  initial begin
+     $dumpfile("dump.vcd");
+      $dumpvars(0,tb_div_5);
+  rst_n =1'b0;
+    repeat(25) @(posedge clk);
+  rst_n =1'b0;
+   repeat(5) @(posedge clk); 
+      data_in=1'b0;
+      @(posedge clk);
+	data_in=1'b1;
+      @(posedge clk);
+	data_in=1'b0;
+      @(posedge clk);
+	data_in=1'b1;
+      @(posedge clk);
+	data_in=1'b0;
+      @(posedge clk);
+	data_in=1'b1;
+      @(posedge clk);
+    data_in=1'b1;
+      @(posedge clk);
+      data_in=1'b1;
+      @(posedge clk);
+      data_in=1'b1;
+      @(posedge clk);
+      data_in=1'b0;
+      @(posedge clk);
+      repeat(100) 
+        begin
+		data_in=$random;
+          @(posedge clk);
+        end
+      end            
+endmodule
+  
