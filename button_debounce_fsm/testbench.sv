aa561f116f6405c76df0d6c743e2a97a63ad1ed2
Humayun Sheikh <58903281+humayunsiraj123@users.noreply.github.com>
2023-03-29 14:01:50 +0500

    Update testbench.sv

--- button_debounce_fsm/testbench.sv
+++ button_debounce_fsm/testbench.sv
@@ -1,26 +1,33 @@
 // Code your testbench here
 // or browse Examples
 
-module testbench;
+
+// Code your testbench here
+// or browse Examples
+
+module tb_btn_debounce;
   logic clk =1'b0;
   logic reset;
   logic btn;
   logic out;
   
   
-  btn_debounce(clk,reset,btn,out);
+  btn_debounce dut(clk,reset,btn,out);
   
   always #5 clk=~clk;
   
   initial begin
     $dumpfile("abc.vcd");
-    $dumpvars(0,a);
+    $dumpvars(0);
     reset =1'b1;
     repeat(25) @(posedge clk);
     reset =1'b0;
     repeat(5) @(posedge clk);
-    repeat(10000) begin
-      btn =$random;
+btn =1'b1;
+	repeat(20) @(posedge clk);
+    
+repeat(100) begin
+      btn =$random();
     end
   end
     endmodule
