e1584fc7aa55e59b0338842546993c98063302aa
Humayun Sheikh <58903281+humayunsiraj123@users.noreply.github.com>
2023-04-26 12:00:00 +0500

    Update bin_to_grey.v

--- DAY3/bin_to_grey.v
+++ DAY3/bin_to_grey.v
@@ -1,16 +1,16 @@
 // Code your design here
 
-module bin_to_grey (
-  input  [4:0] bin,
-  output [4:0] grey
-);
+// module bin_to_grey (
+//   input  [4:0] bin,
+//   output [4:0] grey
+// );
   
-  assign grey[4] = bin[4]           ;
-  assign grey[3] = grey[4] ^ bin[3] ;
-  assign grey[2] = grey[3] ^ bin[2] ;
-  assign grey[1] = grey[2] ^ bin[1] ;
-  assign grey[0] = grey[1] ^ bin[0] ;
-endmodule
+//   assign grey[4] = bin[4]           ;
+//   assign grey[3] = grey[4] ^ bin[3] ;
+//   assign grey[2] = grey[3] ^ bin[2] ;
+//   assign grey[1] = grey[2] ^ bin[1] ;
+//   assign grey[0] = grey[1] ^ bin[0] ;
+// endmodule
 
 
 module bin_to_grey_nbits#(paramter N=8) (
@@ -22,11 +22,11 @@ module bin_to_grey_nbits#(paramter N=8) (
     
   genvar i
   generate
-    for( i =N ;i>0;i=i-1)
-      if(i ==N)
-        grey[N] =bin[N];
+    for( i =N-1 ;i>0;i=i-1)
+      if(i ==N-1)
+        grey[N-1] =bin[N-1];
     else 
-      grey[i]=bin[i+1] ^bin[i];
+      grey[i]=bin[i+1]^bin[i];
   endgenerate
   
 endmodule
